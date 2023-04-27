ca946483ac92602c8ea3506d6ddc4265dc3127fb
LucasButto <lbutto40@gmail.com>
2023-04-27 00:31:50 -0300

    Login & Dashboard Routes implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
 import "./App.css";
-import Main from "./Components/Main/Main";
-import Sidebar from "./Components/Sidebar/Sidebar";
+
+import Layout from "./Layout/Layout";
+import Login from "./Pages/Login/Login";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Main />
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/Pages/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Main from "../../Components/Main/Main";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Main />
+    </div>
+  );
+};
+
+export default Dashboard;
