07d4edbc6481e739e566a24bcf74243f929fbc6f
LucasButto <lbutto40@gmail.com>
2023-05-01 16:21:52 -0300

    Conditional rendering by role type

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,85 +1,34 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-
-import StatesContext from "../../Contexts/StatesContext";
+import React from "react";
+import NavLink from "../NavLink/NavLink";
 
 import "./Sidebar.css";
 
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import BackupTableRoundedIcon from "@mui/icons-material/BackupTableRounded";
-import BuildRoundedIcon from "@mui/icons-material/BuildRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
-import ReceiptRoundedIcon from "@mui/icons-material/ReceiptRounded";
-import ArchiveRoundedIcon from "@mui/icons-material/ArchiveRounded";
-import LeaderboardRoundedIcon from "@mui/icons-material/LeaderboardRounded";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import { NAVLINKS } from "../../Assets/Constants";
+import { USER } from "../../Assets/Constants";
 
 const Sidebar = ({ isVisible, setIsVisible }) => {
-  const navigate = useNavigate();
-  const { setIsLogged } = useContext(StatesContext);
-
-  const handleClick = () => {
+  const hideSidebar = () => {
     setIsVisible(!isVisible);
   };
 
-  const handleLogOut = () => {
-    handleClick();
-    setIsLogged(false);
-    navigate("/login");
-  };
   return (
     <div className={isVisible ? "sidebar open" : "sidebar"}>
       <nav>
         <ul>
-          <li>
-            <Link to="/dashboard" onClick={handleClick}>
-              <DashboardIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Dashboard</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/products" onClick={handleClick}>
-              <BackupTableRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Products</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/purchansing-area" onClick={handleClick}>
-              <ShoppingCartRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Purchansing</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/sales-area" onClick={handleClick}>
-              <ReceiptRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Sales</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/storage-area" onClick={handleClick}>
-              <ArchiveRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Storage</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/dashboard/reports-area" onClick={handleClick}>
-              <LeaderboardRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Reports</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/settings" onClick={handleClick}>
-              <BuildRoundedIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Settings</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/login" onClick={handleLogOut}>
-              <ExitToAppIcon className="sidebar-icon" />
-              <span className="sidebar-link-name">Log Out</span>
-            </Link>
-          </li>
+          {NAVLINKS.map((navlink) => {
+            if (navlink.roles === "All" || navlink.roles === USER.role) {
+              return (
+                <NavLink
+                  navigation={navlink.navigation}
+                  aditionalFunction={hideSidebar}
+                  icon={navlink.icon}
+                  navItemName={navlink.navItemName}
+                />
+              );
+            } else {
+              return null;
+            }
+          })}
         </ul>
       </nav>
     </div>
