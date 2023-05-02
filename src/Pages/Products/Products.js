310243fb8e400d3e59d8c0e0847f7150f0a9b445
juani <juangarnero@hotmail.com.ar>
2023-05-02 00:49:35 -0300

    fast changes

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Table from "../../Components/Tables/Table";
 
 const Products = () => {
   return (
     <div>
       <h2>Products</h2>
-      <Table/>
     </div>
   );
 };
