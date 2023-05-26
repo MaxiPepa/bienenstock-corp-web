abd8c11274e5b1fe343f7b2e3286e0ca0e3865b2
LucasButto <lbutto40@gmail.com>
2023-05-26 01:55:16 -0300

    products render from stock db

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,9 +1,43 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+
+import { parsingDate } from "../../Assets/Parsing";
+
+import { Table } from "../../Assets/Components";
+import { APIContext } from "../../Assets/Contexts";
 
 const Products = () => {
+  const { get } = useContext(APIContext);
+
+  const [products, setProducts] = useState();
+
+  useEffect(() => {
+    get("product/getProductsStock").then((data) => {
+      setProducts(
+        data.products.map((res) => ({
+          productCode: "#" + res.productCode,
+          name: res.name,
+          expirationDate:
+            res.expirationDate === null
+              ? "No expiration date"
+              : parsingDate(res.expirationDate),
+          quantity: res.quantity,
+        }))
+      );
+    });
+  }, [get]);
+
   return (
-    <div>
-      <h2 className="area-title">Products</h2>
+    <div className="area-container">
+      <div className="area-header">
+        <h2 className="area-title">Products</h2>
+      </div>
+      <hr className="division-horizontal-hr" />
+      <section>
+        <Table
+          thead={["Product Code", "Name", "Expiration Date", "Quantity"]}
+          content={products ? products : []}
+        />
+      </section>
     </div>
   );
 };
