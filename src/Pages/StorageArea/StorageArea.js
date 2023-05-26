a17d9b417906d460d96ac66b3be1d63e4216cac2
LucasButto <lbutto40@gmail.com>
2023-05-26 01:55:49 -0300

    pending products working

--- src/Pages/StorageArea/StorageArea.js
+++ src/Pages/StorageArea/StorageArea.js
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { ROLES } from "../../Assets/Constants";
 import { parsingDate, purchaseEntryTableContent } from "../../Assets/Parsing";
 
-import { Modal, Table } from "../../Assets/Components";
+import { Modal, Table, ExpirationsForm } from "../../Assets/Components";
 import { UserContext, APIContext, StatesContext } from "../../Assets/Contexts";
 import { useRedirect } from "../../Assets/Hooks";
 
@@ -18,29 +18,33 @@ const StorageArea = () => {
   const [pendingEntry, setPendingEntry] = useState([]);
   const [cartByIndex, setCartByIndex] = useState([]);
   const [expiration, setExpiration] = useState([]);
-  const [expirationDate, setExpirationDate] = useState([]);
+  const [currentPurchaseId, setCurrectPurchaseId] = useState();
+  const [products, setProducts] = useState();
 
   useEffect(() => {
     get("purchase/getPurchases").then((data) => {
       setPendingEntry(
-        data.purchases.map((r) =>
-          r.pending
-            ? {
-                purchaseId: "#" + r.purchaseId,
-                userFullName: r.userFullName,
-                supplier: r.supplier,
-                totalPrice: "$" + r.totalPrice,
-                date: parsingDate(r.date),
-                pending: "Pending",
-                products: r.products.map((p) => ({
-                  productCode: "#" + p.productCode,
-                  name: p.name,
-                  quantity: p.quantity,
-                  unitPrice: "$" + p.unitPrice,
-                })),
-              }
-            : null
-        )
+        data.purchases
+          .map((r) =>
+            r.pending
+              ? {
+                  purchaseId: "#" + r.purchaseId,
+                  userFullName: r.userFullName,
+                  supplier: r.supplier,
+                  totalPrice: "$" + r.totalPrice,
+                  date: parsingDate(r.date),
+                  pending: "Pending",
+                  products: r.products.map((p) => ({
+                    productId: p.productId,
+                    productCode: "#" + p.productCode,
+                    name: p.name,
+                    quantity: p.quantity,
+                    unitPrice: "$" + p.unitPrice,
+                  })),
+                }
+              : null
+          )
+          .filter((item) => item !== null)
       );
     });
   }, [get]);
@@ -48,15 +52,22 @@ const StorageArea = () => {
   const cartByIndexWithExpiration = (cart) => {
     const falseArray = Array(cart.length).fill(false);
     setExpiration(falseArray);
-    console.log(cart.length);
 
-    return cart.map((p, index) => ({
+    return cart.map(({ productId, ...p }, index) => ({
       ...p,
       expiration: (
         <input
           type="checkbox"
           className="expiration-checkbox"
           checked={expiration[index]}
+          onChange={(e) => {
+            const checked = e.target.checked;
+            setExpiration((prevExpiration) => {
+              const newExpiration = [...prevExpiration];
+              newExpiration[index] = checked;
+              return newExpiration;
+            });
+          }}
         />
       ),
     }));
@@ -64,21 +75,24 @@ const StorageArea = () => {
 
   const confirmEntryProduct = (index) => {
     setCartByIndex(cartByIndexWithExpiration(pendingEntry[index].products));
+    setCurrectPurchaseId(parseInt(pendingEntry[index].purchaseId.substring(1)));
+    setProducts(pendingEntry[index].products);
     setShowModal(true);
   };
 
   const purchaseEntryDataTable = purchaseEntryTableContent(
     pendingEntry,
-    confirmEntryProduct
+    confirmEntryProduct,
+    userData
   );
 
   return (
-    <div className="storage-area">
-      <div className="storage-header">
+    <div className="area-container">
+      <div className="area-header">
         <h2 className="area-title">StorageArea</h2>
       </div>
       <hr className="division-horizontal-hr" />
-      <>
+      <section>
         <h3 className="area-subtitle">Pending products entry</h3>
         <Table
           thead={[
@@ -87,11 +101,11 @@ const StorageArea = () => {
             "Supplier",
             "Total Price",
             "Purchase Date",
-            "Confirm Entry",
+            userData.userType === ROLES.DEPOSITOR ? "Confirm Entry" : null,
           ]}
           content={purchaseEntryDataTable}
         />
-      </>
+      </section>
       <section>
         <h3 className="area-subtitle">Pending products release</h3>
       </section>
@@ -105,6 +119,11 @@ const StorageArea = () => {
             "Expiration?",
           ]}
           content={cartByIndex}
+        />
+        <ExpirationsForm
+          expiration={expiration}
+          currentPurchaseId={currentPurchaseId}
+          products={products}
         />
       </Modal>
     </div>
