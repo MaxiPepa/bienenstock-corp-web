81c307f4dd22848e2959c54b425080d453895ff0
LucasButto <lbutto40@gmail.com>
2023-06-04 20:13:01 -0300

    boxes rendered by user role

--- src/Components/Boxes/Boxes.js
+++ src/Components/Boxes/Boxes.js
@@ -1,11 +1,14 @@
+import { useContext } from "react";
 import { NAVIGATIONSLINKS } from "Assets/Constants";
 
 import BoxItem from "./BoxItem/BoxItem";
+import { UserContext } from "Contexts";
 import { BackupTableRoundedIcon, BuildRoundedIcon } from "Assets/Icons";
 
 import "./Boxes.css";
 
 const Boxes = () => {
+  const { userData } = useContext(UserContext);
   return (
     <div className="dashboard-boxes">
       <div className="boxes-container">
@@ -16,17 +19,17 @@ const Boxes = () => {
           color={"#d94233"}
           navigation={"/dashboard/products"}
         />
-        {NAVIGATIONSLINKS.map((item, index) => {
-          return (
+        {NAVIGATIONSLINKS.map((box, index) => {
+          return box.roles.includes(userData.userType) ? (
             <BoxItem
               key={index}
               quantity="10"
-              title={item.dashboardItemName}
-              icon={item.icon}
-              color={item.color}
-              navigation={item.navigation}
+              title={box.dashboardItemName}
+              icon={box.icon}
+              color={box.color}
+              navigation={box.navigation}
             />
-          );
+          ) : null;
         })}
         <BoxItem
           title="Settings"
