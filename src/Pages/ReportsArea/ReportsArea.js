468dde56453857dcf9fe1242ef8e25e66830a2b7
juani <juangarnero@hotmail.com.ar>
2023-06-13 11:25:04 -0300

    start analyst area

--- src/Pages/ReportsArea/ReportsArea.js
+++ src/Pages/ReportsArea/ReportsArea.js
@@ -1,8 +1,52 @@
+import { useContext,useCallback,useEffect,useState } from "react";
+import { ROLES } from "Assets/Constants";
+
+import { UserContext,APIContext } from "Contexts";
+import { useRedirect } from "Hooks";
+
+import { Table } from "Components";
+
 const ReportsArea = () => {
+
+  const [products,setProducts] = useState();
+
+  const { userData } = useContext(UserContext);
+  const { get } = useContext(APIContext);
+  useRedirect(userData.userType, ROLES.ANALYST);
+  
+  const getProducts = useCallback(() => {
+    get("product/getProductsStock").then((data) => {
+      setProducts(
+        data.products.map((res) => ({
+          productCode: res.productCode,
+          name: res.name,
+        }))
+      );
+    });
+  }, [get]);
+
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   return (
-    <div>
+    <>
       <h2 className="area-title">ReportsArea</h2>
-    </div>
+      <div>
+        <div>
+          <h3>List products</h3>
+          <Table
+            thead={["Product code", "name"]}
+            mapKeys={["productCode" , "name"]}
+            content={products}
+            entity="products"
+          />
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </>
   );
 };
 
