92a31573422d4c875889e153b3309ecc9048b6ab
juani <juangarnero@hotmail.com.ar>
2023-06-19 13:29:43 -0300

    fix error

--- src/Components/Charts/LineCharts.js
+++ src/Components/Charts/LineCharts.js
@@ -33,51 +33,59 @@ var options = {
       x: {
         ticks: { color: 'rgb(0, 0, 0)'}
       }
-  }
+    }
 };
 
-const salesData = [
-    { date: '2023-01-01', purchase: 100 },
-    { date: '2023-02-02', purchase: 150 },
-    { date: '2023-03-03', purchase: 200 },
-    { date: '2023-04-20', purchase: 300 },
-    { date: '2023-05-01', purchase: 200 },
-    { date: '2023-06-15', purchase: 220 },
-    { date: '2023-07-03', purchase: 250 },
-    { date: '2023-08-04', purchase: 280 },
-    { date: '2023-09-05', purchase: 300 },
-    { date: '2023-10-06', purchase: 310 },
-    { date: '2023-11-02', purchase: 320 },
-    { date: '2023-12-09', purchase: 350 },
+const datos =  {
+  labels: ["Enero","febrero","marzo"],
+  datasets: [
+    {
+      label: "Prueba",
+      data: [2,3,5],
+      tension: 0.5,
+      fill : true,
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      pointRadius: 5,
+      pointBorderColor: 'rgba(255, 99, 132)',
+      pointBackgroundColor: 'rgba(255, 99, 132)',
+    }
+  ],
+};
 
-];
 
-const LineCharts = ({axes,title}) => {
+const LineCharts = ({axes,title,dates}) => {
 
-  //const [chartData, setChartData] = useState([]);
-  const [timeRange, setTimeRange] = useState('last3Months'); // Estado para almacenar el rango de tiempo seleccionado
-  const [ axesX , setAxesX] = useState([])
-  const [ axesY , setAxesY] = useState([])
-
+  const [filterDate,setFilterDate] = useState([]);
+  const [values,setValues] = useState([]); 
+  
   const filterChartData = () => {
 
-    
-    // Filtrar los últimos 3 meses
-    let filterData = []
-    const today = new Date();
-    const threeMonthsAgo = new Date(today.getFullYear(), today.getMonth() - 2, today.getDate());
-    filterData = salesData.filter( item => new Date(item.date) >= threeMonthsAgo && new Date(item.date) <= today)
+    let filter = []
+    const startDate = new Date(
+      filterDate.length === 0 ? new Date().getFullYear().toString() : filterDate.startDate.getFullYear().toString(),
+      filterDate.length === 0 ? new Date().getMonth() : filterDate.startDate.getMonth(),
+      filterDate.length === 0 ? new Date().getDate() : filterDate.startDate.getDate()
+    );
 
+    console.log("ingreso en filter data:" +startDate)
   
-    //const labels = timeRange === 'last3Months' ? filterData.map((item) => item.date) : salesData.map((i)=> i.date);
-    //const sales = timeRange === 'last3Months' ? filterData.map((item) => item.purchase) : salesData.map((i)=> i.purchase)
-
+    const endDate = new Date(filterDate.length === 0? 
+      ( new Date().getFullYear(), new Date().getMonth(), new Date().getDate()):
+      ( filterDate.endDate.getFullYear(),filterDate.endDate.getMonth(),filterDate.endDate.getDate())
+    );
+  
+    filter = values.filter( item => new Date(item.date) >= startDate && new Date(item.date) <= endDate) 
+  
+    const labels = filterDate.length === 0 ? values.map((i)=> i.date) : filter.map((i) => i.date) ;
+    const quantity = filterDate.length === 0 ? values.map((i)=> i.quantity) : filter.map((i) => i.quantity)
+  
     return {
-      labels: axesX,
+      labels: labels.reverse(),
       datasets: [
         {
           label: title,
-          data: axesY,
+          data: quantity.reverse(),
           tension: 0.5,
           fill : true,
           borderColor: 'rgb(255, 99, 132)',
@@ -90,32 +98,29 @@ const LineCharts = ({axes,title}) => {
     };
   }
 
+  //console.log(filterChartData())
+  
   const configureAxes = useCallback(() => {
-    setAxesX((axes.dates).reverse())
-    setAxesY((axes.quantity).reverse())
-  },[axes])
-
+    setValues( axes.dates.map((e,i)=>{
+      return {
+        date: e,
+        quantity:axes.quantity[i] 
+      } 
+    }))
+    setFilterDate(dates)
+    console.log(dates)
+  },[axes,dates])
+  
   useEffect(()=>{
     configureAxes()
     console.log("loop")
   },[configureAxes])
 
-  const handleTimeRangeChange = (event) => {
-      setTimeRange(event.target.value);
-      console.log("entro en handle")
-  };    
-  
+
   return (
-      <div>
-          <div>
-          <label htmlFor="timeRange">Rango de tiempo:</label>
-          <select id="timeRange" defaultValue={timeRange} onChange={handleTimeRangeChange}>
-            <option value="all">Todos los tiempos</option>
-            <option value="last3Months">Últimos 3 meses</option>
-          </select>
-        </div>
-        <Line data={ filterChartData() } options={options} />
-      </div>
+    <div>
+      <Line data={datos } options={options} />
+    </div>
   );
 }
 
